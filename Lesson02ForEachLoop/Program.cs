3c0c2514286d9cccb27da1ba30ea8cfba453343b
Nikola Burgić <burgicnikola@gmail.com>
2021-09-20 23:44:54 +0200

    Add lesson 2

---
+++ Lesson02ForEachLoop/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson02ForEachLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[10];
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = i + 10;
+            }
+
+            /*for(int j = 0; j < nums.Length; j++)
+            {
+                Console.WriteLine(nums[j]);
+            }*/
+
+            foreach(int value in nums)
+            {
+                Console.WriteLine(value);
+            }
+
+            string[] friends = { "Djole", "Nidza", "Milan", "Nesa", "Dule" };
+            foreach(string friend in friends)
+            {
+                Console.WriteLine(friend);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson02ForEachLoop/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lesson02ForEachLoop")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lesson02ForEachLoop")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("54db1c72-2aed-4700-810f-e3ddab931959")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
